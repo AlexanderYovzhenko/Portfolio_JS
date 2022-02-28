41c05672bf0fdcea86659e5c100ed4106ffa9d3d
Alexander <renyreny125tt@gmail.com>
2022-02-28 15:46:52 +0300

    feat: add scroll animation

--- src/js/main.js
+++ src/js/main.js
@@ -15,12 +15,53 @@ const checkClick = () => {
 
 const photo = document.querySelector('.photo');
 
-photo.addEventListener('mouseover', (event)=> {
-  event.target.style.transform = 'scale(150%)';
+photo.addEventListener('click', (event)=> {
+  event.target.style.transition = '.3s';
+  console.log(event.target.style.transform)
+  if (event.target.style.transform === 'scale(1.5)') {
+    event.target.style.transform = 'scale(100%)';
+  } else {
+    event.target.style.transform = 'scale(150%)';
+  }
 });
 
-photo.addEventListener('mouseout', (event) => {
-  event.target.style.transform = 'scale(100%)';
-});
+
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    function  animOnScroll() {
+      for (let index = 0; index < animItems.length; index++) {
+        const animItem = animItems[index];
+        const animItemHeight = animItem.offsetHeight;
+        const animItemOffset = offset(animItem).top;
+        const animStart = 4;
+
+        let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+        if (animItemHeight > window.innerHeight) {
+          animItemPoint = window.innerHeight - window.innerHeight / animStart;
+        }
+
+        if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+          animItem.classList.add('_active');
+        } else {
+          if (!animItem.classList.contains('_anim-no-hide')) {
+            animItem.classList.remove('_active');
+          }
+        }
+      }
+    }
+    function offset(el) {
+      const rect = el.getBoundingClientRect();
+      const scrollLeft = window.pageYOffset || document.documentElement.scrollLeft;
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+      return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+    }
+    setTimeout(() => {
+      animOnScroll();
+    }, 300);
+}
 
 checkClick();
